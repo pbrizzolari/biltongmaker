83359db092ed7a4cb31a6f9a77a45e9fc8c8b399
Paolo Brizzolari <pbrizzolari@gmail.com>
2020-08-01 09:32:40 +0200

    feat: Read fan speed from the PWM fan
    Added in the code to read the fan speed and output it to the serial console.

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ I've used the dht22 sensor because that's what I had, but the dht11 sensor shoul
 I've used the adafruit DHT library, but there are other libraries as well.
 
 The nice thing about this sensor is being able to read the temperature and humidity off of a single data pin which is especially useful if you're constrained on pins.
+
+<h2>Part 2 - Reading the PWM Fan Speed</h2>
+
+Reading the PWM fan requires that you use an interrupt on the read pin to count how many times the hall sensor in the fan is triggered. Once you have that value you have to divide it by two since the sensor triggers twice per revolution, then divide it by the interval that you're reading at before lastly multiplying the value by 60 to get into minutes. One issue I had with reading the fan speed is that this particular board (ESP8266 NodeMCU) doesn't have hardware PWM and so sometimes the fan control messed with reading the fan speed. 
--- biltongmaker.ino
+++ biltongmaker.ino
@@ -17,6 +17,56 @@ float h = 0.0;
 unsigned long previousMillis = 0;    // will store last time DHT was updated
 const long interval = 2000;
 
+void displayDHTDetails(){
+  // Read temperature as Celsius (the default)
+  float newT = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float newT = dht.readTemperature(true);
+  // if temperature read failed, don't change the value
+  if (isnan(newT)) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  else {
+    t = newT;
+    Serial.println(t);
+  }
+  // Read Humidity
+  float newH = dht.readHumidity();
+  // if humidity read failed, don't change h value 
+  if (isnan(newH)) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  else {
+    h = newH;
+    Serial.println(h);
+  }
+}
+
+//FAN Measurement (PWM Fan)
+#define FANREAD 4
+volatile int interruptCounter; //counter use to detect hall sensor in fan
+int RPM;                      //variable used to store computed value of fan RPM
+
+//This is the function called by the interrupt.
+void ICACHE_RAM_ATTR handleInterrupt() { 
+    interruptCounter++;
+
+}
+
+void computeFanSpeed() {
+    //interruptCounter counts 2 pulses per revolution of the fan over a one second period.
+    //We also need to divide by the interval in seconds so that as the interval changes 
+    //it doesn't ruin the calculation.
+    int count = interruptCounter;
+    interruptCounter = 0;
+    RPM =  count/2/(interval/1000) * 60 ;
+}
+
+void displayFanSpeed() {
+    Serial.print(RPM, DEC);        //Prints the computed fan speed to the serial monitor
+    Serial.print(" RPM\r\n");      //Prints " RPM" and a new line to the serial monitor
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   
@@ -25,35 +75,26 @@ void setup() {
 
   //Start up the DHT Sensor
   dht.begin();
+
+  //Setup the interrupt to read the fan speed
+  pinMode(FANREAD, INPUT_PULLUP);
+  interruptCounter = 0;
+  RPM = 0;
+  attachInterrupt(FANREAD, handleInterrupt, FALLING);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   unsigned long currentMillis = millis(); //We check the milliseconds each time so that we only read the sensor every <interval> seconds.
   if (currentMillis - previousMillis >= interval) {
-    // save the last time you updated the DHT values
+    // save the last time you updated the values
     previousMillis = currentMillis;
-    // Read temperature as Celsius (the default)
-    float newT = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    //float newT = dht.readTemperature(true);
-    // if temperature read failed, don't change the value
-    if (isnan(newT)) {
-      Serial.println("Failed to read from DHT sensor!");
-    }
-    else {
-      t = newT;
-      Serial.println(t);
-    }
-    // Read Humidity
-    float newH = dht.readHumidity();
-    // if humidity read failed, don't change h value 
-    if (isnan(newH)) {
-      Serial.println("Failed to read from DHT sensor!");
-    }
-    else {
-      h = newH;
-      Serial.println(h);
-    }
+
+    displayDHTDetails();
+
+    //Calculate and print out the fan speed
+    computeFanSpeed();
+    displayFanSpeed();
   }
+  yield();
 }
